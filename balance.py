9eac18c84cf97f059d5770270cef760a9ea924ed
fdyytt <kentosrtobgl6@gmail.com>
2025-02-12 08:09:03 +0700

    Update balance.py

--- balance.py
+++ balance.py
@@ -1,21 +1,27 @@
-from MultipleFiles.database import Database
+from database import Database
 
 class Balance:
     def __init__(self, db):
         self.db = db
-        self.collection = db.get_collection("balance")
+        self.collection = "balances"
 
-    def get_balance(self, grow_id):
-        return self.collection.find_one({"grow_id": grow_id})
+    def get_balance(self, grow_id, admin_id):
+        balance_data = self.db.find(self.collection, {"grow_id": grow_id, "admin_id": admin_id})
+        if balance_data:
+            return balance_data[0]
+        else:
+            return {"grow_id": grow_id, "balance": 0, "admin_id": admin_id}
 
-    def update_balance(self, grow_id, jumlah_donasi, mata_uang_donasi):
+    def update_balance(self, grow_id, admin_id, jumlah_donasi, mata_uang_donasi):
         konversi = {
             'WL': 1,
             'DL': 100,
             'BGL': 10000
         }
         jumlah_donasi_wl = jumlah_donasi * konversi.get(mata_uang_donasi, 0)
-        self.collection.update_one({"grow_id": grow_id}, {"$inc": {"balance": jumlah_donasi_wl}}, upsert=True)
+        current_balance = self.get_balance(grow_id, admin_id)["balance"]
+        new_balance = current_balance + jumlah_donasi_wl
+        self.db.update(self.collection, {"grow_id": grow_id, "admin_id": admin_id}, {"balance": new_balance})
 
-    def add_balance(self, grow_id, balance):
-        self.collection.insert_one({"grow_id": grow_id, "balance": balance})
+    def add_balance(self, grow_id, admin_id, balance):
+        self.db.insert(self.collection, {"grow_id": grow_id, "balance": balance, "admin_id": admin_id})
