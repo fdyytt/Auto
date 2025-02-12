c9f95fe42a188731eb18a85e6936174d110c7716
fdyytt <kentosrtobgl6@gmail.com>
2025-02-12 19:04:37 +0700

    Delete cog/config.py

--- cog/config.py
+++
@@ -1,7 +0,0 @@
-def load_config(file_path):
-    config = {}
-    with open(file_path, 'r') as file:
-        for line in file:
-            key, value = line.strip().split('=')
-            config[key] = value
-    return config
