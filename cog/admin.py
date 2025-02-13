c5dd75bba2df669bcb2f08e6a7fac2f096347ad5
fdyytt <kentosrtobgl6@gmail.com>
2025-02-13 11:42:18 +0700

    Update admin.py

--- cog/admin.py
+++ cog/admin.py
@@ -1,8 +1,7 @@
 import discord
 from discord.ext import commands
-from live import db, BuyButton
+from live import db, BuyButton, Database  # Pastikan Database diimpor dari live.py
 from discord.ui import View
-from main import config
 
 class AdminCommands(commands.Cog):
     def __init__(self, bot, db):
@@ -16,7 +15,7 @@ class AdminCommands(commands.Cog):
     async def add_product(self, ctx, name_product: str, price: float):
         if self.is_admin(ctx):
             try:
-                product = {"name": name_product, "price": price, "stock": 0, "description": "", "admin_id": ctx.author.id}
+                product = {"name": name_product, "price": price, "stock": 0, "description": ""}
                 self.db.insert("products", product)
                 await ctx.send(f'Product {name_product} added with price {price}!')
             except Exception as e:
@@ -57,74 +56,9 @@ class AdminCommands(commands.Cog):
     async def set_description(self, ctx, name_product: str, description: str):
         if self.is_admin(ctx):
             self.db.update("products", {"name": name_product, "admin_id": ctx.author.id}, {"description": description})
-            await ctx.send(f'Description for {name_product} updated!')
+            await ctx.send(f'Description for {name_product} set to {description}!')
         else:
             await ctx.send("Anda tidak memiliki akses admin.")
-
-    @commands.command(name='setWorld')
-    async def set_world(self, ctx, world: str, owner: str, bot: str):
-        if self.is_admin(ctx):
-            world_info = {"world": world, "owner": owner, "bot": bot, "admin_id": ctx.author.id}
-            self.db.update("world_info", {"world": world, "admin_id": ctx.author.id}, world_info)
-            await ctx.send(f'World information updated!')
-        else:
-            await ctx.send("Anda tidak memiliki akses admin.")
-
-    @commands.command(name='send')
-    async def send_product(self, ctx, grow_id: str, product: str, count: int):
-        if self.is_admin(ctx):
-            product_info = self.db.find_product("products", {"name": product, "admin_id": ctx.author.id})
-            if product_info and product_info[3] >= count:
-                new_stock = product_info[3] - count
-                self.db.update("products", {"name": product, "admin_id": ctx.author.id}, {"stock": new_stock})
-                await ctx.send(f'Sent {count} {product} to {grow_id}!')
-            else:
-                await ctx.send(f'Not enough stock for {product}!')
-        else:
-            await ctx.send("Anda tidak memiliki akses admin.")
-
-    @commands.command(name='addBal')
-    async def add_balance(self, ctx, grow_id: str, balance: float):
-        if self.is_admin(ctx):
-            user_info = self.db.find("balances", {"grow_id": grow_id, "admin_id": ctx.author.id})
-            if user_info:
-                new_balance = user_info[0][1] + balance
-                self.db.update("balances", {"grow_id": grow_id, "admin_id": ctx.author.id}, {"balance": new_balance})
-                await ctx.send(f'Balance for {grow_id} updated to {new_balance}!')
-            else:
-                await ctx.send(f'User {grow_id} not found!')
-        else:
-            await ctx.send("Anda tidak memiliki akses admin.")
-
-    @commands.command(name='reduceBal')
-    async def reduce_balance(self, ctx, grow_id: str, balance: float):
-        if self.is_admin(ctx):
-            user_info = self.db.find("balances", {"grow_id": grow_id, "admin_id": ctx.author.id})
-            if user_info:
-                new_balance = user_info[0][1] - balance
-                self.db.update("balances", {"grow_id": grow_id, "admin_id": ctx.author.id}, {"balance": new_balance})
-                await ctx.send(f'Balance for {grow_id} updated to {new_balance}!')
-            else:
-                await ctx.send(f'User {grow_id} not found!')
-        else:
-            await ctx.send("Anda tidak memiliki akses admin.")
-
-    @commands.command(name='stock')
-    async def stock(self, ctx, id_live_stock: int):
-        channel = self.bot.get_channel(id_live_stock)
-        produk_data = self.db.get_all_products("products", ctx.guild.owner_id)
-        user_balance = self.db.get_user_balance(ctx.author.id, ctx.guild.owner_id)
-        embed = discord.Embed(title='Live Stock', description=f'Saldo Anda: :WL: {user_balance}')
-        for produk in produk_data:
-            embed.add_field(name=produk[1], value=f'Harga: {produk[2]}\nStock: {produk[3]}', inline=False)
-        view = View()
-        for produk in produk_data:
-            button = BuyButton(label=produk[1], product_id=produk[0])
-            view.add_item(button)
-        await channel.send(embed=embed, view=view)
-
-    async def cog_check(self, ctx):
-        return self.is_admin(ctx)
 
 async def setup(bot):
     await bot.add_cog(AdminCommands(bot, db))
